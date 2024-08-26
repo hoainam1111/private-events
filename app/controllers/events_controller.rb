09ca412497c2f92d6f145962d5d351f88259bf43
hoainam1111 <66334691+hoainam1111@users.noreply.github.com>
2024-08-26 10:10:09 +0700

    Events, users and and some features around them

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,23 @@
+class EventsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+  def index
+    @events = Event.all
+  end
+  def new
+    @event = Event.new
+  end
+  def create
+    @event = current_user.events.build(event_params)
+    if @event.save
+      redirect_to root_path, notice: 'Event was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:date, :other_attributes)
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  get "users/show"
+  devise_for :users
+  root to: 'events#index'
+  resources :events
+end
