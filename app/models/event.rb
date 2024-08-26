f92950aac8d026317dfb600999f221be32014d45
hoainam1111 <66334691+hoainam1111@users.noreply.github.com>
2024-08-26 12:25:30 +0700

    use scope

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,12 +2,10 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User', foreign_key: 'creator_id'
   has_many :attendances
   has_many :attendees, through: :attendances, source: :user
-  def self.past
-    where('date < ?', Date.today).order(date: :desc)
-  end
+  # Scope to get past events
+  scope :past, -> { where('date < ?', Date.today).order(date: :desc) }
 
-  # Class method to get upcoming events
-  def self.upcoming
-    where('date >= ?', Date.today).order(date: :asc)
-  end
+  # Scope to get upcoming events
+  scope :upcoming, -> { where('date >= ?', Date.today).order(date: :asc) }
+  
 end
