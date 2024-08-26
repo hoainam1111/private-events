93697453cb6d51afa1347c145b73ca8c6abb9b61
hoainam1111 <66334691+hoainam1111@users.noreply.github.com>
2024-08-26 12:04:51 +0700

    separate index events into past and future

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,12 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User', foreign_key: 'creator_id'
   has_many :attendances
   has_many :attendees, through: :attendances, source: :user
+  def self.past
+    where('date < ?', Date.today).order(date: :desc)
+  end
+
+  # Class method to get upcoming events
+  def self.upcoming
+    where('date >= ?', Date.today).order(date: :asc)
+  end
 end
