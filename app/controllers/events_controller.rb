f41b4dad237855ecd47da08b002b7de191fe7320
hoainam1111 <66334691+hoainam1111@users.noreply.github.com>
2024-08-26 12:44:46 +0700

    edit delete

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,27 +1,56 @@
 class EventsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :set_event, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :authorize_user!, only: [:edit, :update, :destroy]
+
   def index
     @past_events = Event.past
     @upcoming_events = Event.upcoming
   end
+
   def show
-    @event = Event.find(params[:id])
   end
+
   def new
-    @event = current_user.events.build
+    @event = Event.new
   end
+
   def create
-    @event = current_user.events.build(event_params)
+    @event = current_user.events_created.build(event_params)
     if @event.save
-      redirect_to root_path, notice: 'Event was successfully created.'
+      redirect_to @event, notice: 'Event was successfully created.'
     else
       render :new
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @event.update(event_params)
+      redirect_to @event, notice: 'Event was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @event.destroy
+    redirect_to events_path, notice: 'Event was successfully deleted.'
+  end
+
   private
 
+  def set_event
+    @event = Event.find(params[:id])
+  end
+
   def event_params
-    params.require(:event).permit(:date, :other_attributes)
+    params.require(:event).permit(:date)
+  end
+
+  def authorize_user!
+    redirect_to events_path, alert: 'Not authorized' unless @event.creator == current_user
   end
 end
