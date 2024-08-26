8f225b007d958b168123167123de6db9bbff6d7c
hoainam1111 <66334691+hoainam1111@users.noreply.github.com>
2024-08-26 11:17:31 +0700

    fix can't log_out

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  get "users/show"
+
   devise_for :users
+  devise_scope :user do  
+    get '/users/sign_out' => 'devise/sessions#destroy'     
+  end
   root to: 'events#index'
   resources :events
+  resources :users
 end
